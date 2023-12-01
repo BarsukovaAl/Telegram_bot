ef61fcfa9239886f6fd748c4f4c631bc741b9e28
BarsukovaAl <148317152+BarsukovaAl@users.noreply.github.com>
2023-12-01 16:14:12 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -16,5 +16,14 @@ def write_to_db(userid, name, surname, birthday):
         file.write('\n')
 
 
+def find_user_by_id(userid):
+    logger.info(f'вызвана функция find_user_by_id: {userid}')
+    with open('database.txt', 'r', encoding='UTF-8') as file:
+        for line in file:
+            user_data = line.split('\t')
+            if user_data[0] == str(userid):
+                return user_data
+
+
 if __name__ == '__main__':
     write_to_db('Барсук', 'Алёна', 'Барсукова', ' 04.07.2007')
