79ccc5bbf2ed3e17fc5c1a94e87973d336255dcc
BarsukovaAl <148317152+BarsukovaAl@users.noreply.github.com>
2023-12-01 16:13:20 +0300

    Add files via upload

--- fsm.py
+++ fsm.py
@@ -1,8 +1,6 @@
-from telegram import Update, ParseMode
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, Dispatcher
-from telegram.ext import MessageHandler, CommandHandler, CallbackQueryHandler, ConversationHandler
+from telegram import Update
+from telegram import ReplyKeyboardRemove
+from telegram.ext import MessageHandler, CommandHandler, ConversationHandler
 from telegram.ext import CallbackContext
 from telegram.ext import Filters
 
@@ -11,9 +9,27 @@ from db import write_to_db
 
 logger = logging.getLogger(__name__)
 
-WAIT_NAME, WAIT_SURNAME, WAIT_BIRTHDAY = range(3)
+WAIT_OK, WAIT_NAME, WAIT_SURNAME, WAIT_BIRTHDAY = range(3)
 
 
+
+def check_register(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    logger.info(f'{username=} {user_id=} вызвал функцию check_register')
+    user = find_user_by_id(user_id)
+    if not usesr:
+        return ask_name(update,context)
+    answer = [
+        f'Привет!',
+        f'Ты уже зарегистрирован со следующими данными:\n',
+        f'Имя: {user[1]}',
+        f'Фамилия: {user[2]}',
+        
+
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
 def ask_name(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     username = update.message.from_user.username
@@ -33,7 +49,7 @@ def get_name(update: Update, context: CallbackContext):
     username = update.message.from_user.username
     text = update.message.text
     logger.info(f'{username=} {user_id=} вызвал функцию get_name')
-    write_to_db()
+    write_to_db('userid', 'name', 'surname', 'birthday')
     answer = [
         f'Твое имя - {text}'
     ]
